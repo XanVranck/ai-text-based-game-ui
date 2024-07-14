b03756b02c919f3c82e5d3b386a2296666a99288
xanv <vranckaert.xan@gmail.com>
2024-07-14 21:27:59 +0200

    better css

--- dalle.js
+++ dalle.js
@@ -6,7 +6,7 @@ const openai = new OpenAI();
 const callDallE = async function (prompt) {
     const response = await openai.images.generate({
         model: "dall-e-3",
-        prompt: "Can you generare an image for a adventure game, this is the scenario: " + prompt,
+        prompt: "Can you generare an image for a adventure game, this is the scenario with 2 possible options: " + prompt,
         n: 1,
         size: "1024x1024",
     });
